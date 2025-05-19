f52dfe44301ed952717ff1d33779063044c2eee9
MinaRefat123 <monmonreg@gmail.com>
2025-05-19 02:57:36 +0200

    server

--- web/web/mern/server/web/web/mern/server/Routers/doctorRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { setAvailability, getAvailability } = require('../Controllers/doctorController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/availability', authMiddleware, setAvailability);
-router.get('/availability/:doctorId', authMiddleware, getAvailability);
-
-module.exports = router;
